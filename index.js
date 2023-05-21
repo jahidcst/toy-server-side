e90b7022313846636d5254772f38d1cfd70dc157
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-05-21 10:01:41 +0600

    mongo work

--- index.js
+++ index.js
@@ -26,12 +26,56 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        const toyCollection = client.db('toyDB').collection('toys')
+
+        // app.get('/hello', (req, res) =>{
+        //     res.send('all is well')
+        // })
+
+        // post to toyDb
+        app.post('/allToys', async(req, res) =>{
+            const newToy = req.body
+            const result = await toyCollection.insertOne(newToy)
+            res.send(result)
+            
+        });
+
+        // get from toyDB
+        app.get('/allToys', async(req, res) =>{
+            const myToys = await toyCollection.find().toArray()
+            res.send(myToys)
+        })
+
+        // get single Data
+        app.get('/allToys/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toyCollection.findOne(query)
+            res.send(result)
+        })
+        // delete from toyDB
+        app.delete('/allToys/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toyCollection.deleteOne(query)
+            res.send(result)
+        });
+
+        // patch to toyDB
+        app.patch('/allToys/:id', async(req, res) =>{
+            const id = res.params.id
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
